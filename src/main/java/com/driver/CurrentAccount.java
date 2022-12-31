220489677d1d28bfd05d0f6d7ffc4d52d190d8fd
Aqeeb <aqebmohammad@gmail.com>
2022-12-31 09:51:25 +0530

    runCode

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,45 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name,balance,5000);
+        if (balance<5000){
+            throw new Exception("Insufficient Balance");
+        }
+        this.tradeLicenseId=tradeLicenseId;
+    }
+    private String valid(String S){
+        int[] hash = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            hash[S.charAt(i) - 'a']++;
+        }
+        int max = 0, letter = 0;
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] > max) {
+                max = hash[i];
+                letter = i;
+            }
+        }
+        if (max > (S.length() + 1) / 2) {
+            return "";
+        }
+        char[] res = new char[S.length()];
+        int idx = 0;
+        while (hash[letter] > 0) {
+            res[idx] = (char) (letter + 'a');
+            idx += 2;
+            hash[letter]--;
+        }
+        for (int i = 0; i < hash.length; i++) {
+            while (hash[i] > 0) {
+                if (idx >= res.length) {
+                    idx = 1;
+                }
+                res[idx] = (char) (i + 'a');
+                idx += 2;
+                hash[i]--;
+            }
+        }
+        return String.valueOf(res);
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,6 +51,18 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        for(int i=0;i<tradeLicenseId.length()-1;i++){
+            if(tradeLicenseId.charAt(i)==tradeLicenseId.charAt(i+1)){
+                String s = valid(tradeLicenseId);
+                if(s.equals("")){
+                    throw new Exception("Valid License can not be generated");
+                }
+                else {
+                    tradeLicenseId=s;
+                }
+                break;
+            }
+        }
 
     }
 
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -5,24 +5,34 @@ public class SavingsAccount extends BankAccount{
     double maxWithdrawalLimit;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
+        super(name,balance,0);
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+        this.rate = rate;
         // minimum balance is 0 by default
-
     }
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-
+        if(amount > maxWithdrawalLimit)
+            throw new Exception("Maximum Withdraw Limit Exceed");
+        if(amount > getBalance())
+            throw new Exception("Insufficient Balance");
+            setBalance(getBalance()-amount);
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
-
+        double interest =(getBalance()*rate*years)/100;
+        return interest;
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+        double i = (double) times*years;
+        double j = (rate/times)+1;
+        j = Math.pow(j,i);
+        return j*getBalance();
     }
 
 }
